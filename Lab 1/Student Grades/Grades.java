12e633437ea352ed01dc9451799b1871a7d116bf
11jwp03 <11swp03@gmail.com>
2022-09-14 09:17:54 -0400

    added two names

--- Lab 1/Student Grades/Grades.java
+++ Lab 1/Student Grades/Grades.java
@@ -10,5 +10,8 @@ public class Grades {
         System.out.println ("\t----\t---\t-----\t-----");
         System.out.println ("\tBob\t20\t5\t25");
         System.out.println("\tRobert\t40\t7\t47");
+        System.out.println("\tJane\t38\t7\t45");
+        System.out.println("\tSophia\t43\t7\t50");
+        System.out.println("\tSam\t41\t4\t45");
         System.out.println ();
 }
