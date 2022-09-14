28859b054b79543e60f7364394b62ab540f32c49
11jwp03 <11swp03@gmail.com>
2022-09-14 09:39:13 -0400

    added missing bracket

---
+++ Lab 1/Errors.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+// File:     Errors.java
+// Purpose: A program with lots of syntax errors
+// Correct all of the errors (STUDY the program carefully!!)
+public class Errors
+{
+public static void main (String[] args)
+{
+
+int Name; //name of the user
+int number;
+int numSq;
+Scanner scan = new Scanner(System.in);
+System.out.print ("Enter your name, please: ");
+Name = scan.nextInt();
+System.out.print ("What is your favorite number?");
+number = scan.nextInt();
+numSq = number * number;
+System.out.println (Name + " , the square of your number is "
+                    + "numSquared");
+    }
+}
