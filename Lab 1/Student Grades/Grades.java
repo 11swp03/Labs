7033d86b527cafd686eb42c2631c71e4a12be980
11jwp03 <11swp03@gmail.com>
2022-09-14 09:06:31 -0400

    changed header

---
+++ Lab 1/Student Grades/Grades.java
@@ -0,0 +1,12 @@
+package Student Grades;
+
+public class Grades {
+    public static void main (String[] args) {
+        System.out.println ();
+        System.out.println (" \t/////////////\\\\\\\\\\\t") ;
+        System.out.println ("\t==\t\tStudentPoitns\t\t==\t");
+        System.out.println ("\tJohn\t\tEffingham\tCS&E");
+        System.out.println ("\tFred\t\tHartford\tCS&E");
+        System.out.println ("\tBob\t\tMeriden\t\tMath");
+        System.out.println ();
+}
