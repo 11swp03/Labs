c50cfbf3e789619721d57181a6c85d8030d7e22f
11jwp03 <11swp03@gmail.com>
2022-09-14 08:44:47 -0400

    Added new collum and third person

---
+++ Lab 1/Lab1.java
@@ -0,0 +1,14 @@
+public class Lab1 {
+    // --------------------------
+    //  main prints the list
+    // --------------------------
+    public static void main (String[] args) {
+        System.out.println ();
+        System.out.println (" \tName\t\tHometown\tMajor") ;
+        System.out.println ("\t====\t\t========");
+        System.out.println ("\tJohn\t\tEffingham\tCS&E");
+        System.out.println ("\tMoui\tWashington\tCS&E");
+        System.out.println("\tBob\tMeriden\tMath");
+        System.out.println ();
+    }
+}
