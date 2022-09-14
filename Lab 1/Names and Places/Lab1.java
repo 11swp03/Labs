56d1497604b3649fef1f6282c91f7195a1fb941f
11jwp03 <11swp03@gmail.com>
2022-09-14 08:58:13 -0400

    changed name

--- Lab 1/Names and Places/Lab1.java
+++ Lab 1/Names and Places/Lab1.java
@@ -7,7 +7,7 @@ public class Lab1 {
         System.out.println (" \tName\t\tHometown\tMajor") ;
         System.out.println ("\t====\t\t========");
         System.out.println ("\tJohn\t\tEffingham\tCS&E");
-        System.out.println ("\tMoui\t\tHartford\tCS&E");
+        System.out.println ("\tFred\t\tHartford\tCS&E");
         System.out.println ("\tBob\t\tMeriden\t\tMath");
         System.out.println ();
     }
