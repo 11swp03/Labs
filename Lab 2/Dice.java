b40473f53883762cdec513b3bac78727b8ed55a1
11jwp03 <11swp03@gmail.com>
2022-09-20 19:28:29 -0400

    add second die and total

--- Lab 2/Dice.java
+++ Lab 2/Dice.java
@@ -4,9 +4,13 @@ public class Dice {
     public static void main(String[] args) {
     int die1;
     int die2;
+    int Tdice;
 
-    Random Genorator = new Random();
-    die1 = Genorator.nextInt(5) + 1;
+    Random Genorator1 = new Random();
+    Random Genorator2 = new Random();
+    die1 = Genorator1.nextInt(5) + 1;
+    die2 = Genorator2.nextInt(5) + 1;
+    Tdice = (die1 + die2);
 
 
     }
