b08577970eab7a1c50e356df6580f462ec08f950
11jwp03 <11swp03@gmail.com>
2022-09-20 19:26:03 -0400

    init set up

--- Lab 2/Dice.java
+++ Lab 2/Dice.java
@@ -1,5 +1,13 @@
-package Lab 2;
+import java.util.Random;
 
 public class Dice {
-    
+    public static void main(String[] args) {
+    int die1;
+    int die2;
+
+    Random Genorator = new Random();
+    die1 = Genorator.nextInt(5) + 1;
+
+
+    }
 }
