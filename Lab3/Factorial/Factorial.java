0c513a2e6eefe71b760496d08d1c07f2e8233099
11jwp03 <11swp03@gmail.com>
2022-09-28 08:21:50 -0400

    scan variable

--- Lab3/Factorial/Factorial.java
+++ Lab3/Factorial/Factorial.java
@@ -3,5 +3,14 @@ import java.util.Scanner;
 public class Factorial {
 
     public static void main(String[] args) {
+        int factor;
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Factorial Calculator");
+        System.out.println();
+        System.out.print("Enter a positive integer: ");
+        factor = input.nextInt();
+        
     }
 }
