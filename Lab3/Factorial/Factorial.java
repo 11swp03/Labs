747b41d36a5a559a0efee3d54d7cfc485e6db7fc
11jwp03 <11swp03@gmail.com>
2022-09-28 08:55:12 -0400

    last part test 1

--- Lab3/Factorial/Factorial.java
+++ Lab3/Factorial/Factorial.java
@@ -7,24 +7,26 @@ public class Factorial {
         int i;
         int factor = 1;
 
-        Scanner input = new Scanner (System.in);
+		do {
+			Scanner input = new Scanner (System.in);
 
-        System.out.println("Factorial Calculator");
-        System.out.println();
-        System.out.print("Enter a positive integer: ");
-        i = input.nextInt();
+			System.out.println("Factorial Calculator");
+			System.out.println();
+			System.out.print("Enter a positive integer: ");
+			i = input.nextInt();
 
-        if (i>0) {
-            	for(n = 1;n <= i ;n++){
-            	factor *= n;
-            	}
-			System.out.println("The Factorial of " + i + " is " + factor);
-        } else if (i<0){
-            System.out.println("Please enter a positive integer");
-        } else { 
-			factor = 1;
-			System.out.println("The Factorial of " + i + " is " + factor);
-		}
+			if (i>0) {
+					for(n = 1;n <= i ;n++){
+					factor *= n;
+					}
+				System.out.println("The Factorial of " + i + " is " + factor);
+			} else if (i<0){
+				System.out.println("Please enter a positive integer");
+			} else { 
+				factor = 1;
+				System.out.println("The Factorial of " + i + " is " + factor);
+			}
+		} while (i<0);
     
     }
 }
