970f9c2840dc808eab279fe739481065d8fccf5a
11jwp03 <11swp03@gmail.com>
2022-10-26 09:00:09 -0400

    move to class add space

--- Lab 5/Cards/Cards.java
+++ Lab 5/Cards/Cards.java
@@ -1,4 +1,4 @@
-public class Rank{
+public class Cards {
     enum Rank {
         ace,
         two,
@@ -14,9 +14,6 @@ public class Rank{
         queen,
         king
     }
-}
-
-public class Cards {
     public static void main(String[] args) {
         Rank highCard, faceCard, card1, card2;
         highCard = Rank.ace;
@@ -24,6 +21,6 @@ public class Cards {
         card1 = Rank.six;
         card2 = Rank.nine;
 
-        System.out.println("A two card hand: " + card1 + card2);
+        System.out.println("A two card hand: " + card1 + " " + card2);
     }
 }
